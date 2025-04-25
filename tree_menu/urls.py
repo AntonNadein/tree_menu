004f89c8e7fa05b1a4ab19e91f038f4d468c63e6
Anton <fynjy-666@mail.ru>
2025-04-25 13:16:30 +0300

    add start template + view

--- tree_menu/urls.py
+++ tree_menu/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+
 from tree_menu.apps import TreeMenuConfig
+from . import views
 
 app_name = TreeMenuConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("index/", views.Index.as_view(), name="index"),
+]
--- tree_menu/views.py
+++ tree_menu/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Index(TemplateView):
+    """Представление домашняя страница"""
+
+    template_name = "tree_menu/index.html"
