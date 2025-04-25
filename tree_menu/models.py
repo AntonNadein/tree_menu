82b8e0e8e70fb1471c697f3343fba183d1a79784
Anton <fynjy-666@mail.ru>
2025-04-25 11:47:52 +0300

    add model tree_menu

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class TreeMenu(models.Model):
+    """ Модель древовидного меню """
+
+    name = models.CharField(max_length=75, verbose_name='Название пункта')
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name='Пункт меню для ответвлений'
+    )
+    menu_name = models.CharField(max_length=100, verbose_name='Название меню')
+    url = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Ссылка(URL) или именованная ссылка(named URL)'
+    )
+    named_url = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Именованная ссылка(named URL)'
+    )
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
+
+    def __str__(self):
+        return self.name
