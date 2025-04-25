f1ce8948e26df68d771692c486c0608cb8f6880a
Anton <fynjy-666@mail.ru>
2025-04-25 12:31:18 +0300

    add admin tree_menu

--- tree_menu/admin.py
+++ tree_menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from tree_menu.models import TreeMenu
+
+
+@admin.register(TreeMenu)
+class TreeMenuAdmin(admin.ModelAdmin):
+    """ Админ панель для древовидного меню """
+    list_display = ('name', 'menu_name', 'parent', 'url', 'named_url',)
+    list_filter = ('menu_name',)
+    search_fields = ('name', 'menu_name', 'url', 'named_url')
+    ordering = ("menu_name",)
+    list_display_links = ("name",)
+    list_per_page = 20
