c04a89850806e5c8f22c0527ac80d6a8b98d556e
Anton <fynjy-666@mail.ru>
2025-04-25 22:16:13 +0300

    upd template

--- tree_menu/urls.py
+++ tree_menu/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = TreeMenuConfig.name
 
 urlpatterns = [
-    path("index/", views.Index.as_view(), name="index"),
+    path("", views.Index.as_view(), name="index"),
     path("page/<int:pk>/", views.PageDetail.as_view(), name="page"),
     path("list/", views.ListDetail.as_view(), name="list"),
 ]
