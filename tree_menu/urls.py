cce7e919038891e7241a30912820bc2f97ddeeb5
Anton <fynjy-666@mail.ru>
2025-04-25 21:22:24 +0300

    add test view

--- tree_menu/urls.py
+++ tree_menu/urls.py
@@ -7,4 +7,6 @@ app_name = TreeMenuConfig.name
 
 urlpatterns = [
     path("index/", views.Index.as_view(), name="index"),
+    path("page/<int:pk>/", views.PageDetail.as_view(), name="page"),
+    path("list/", views.ListDetail.as_view(), name="list"),
 ]
--- tree_menu/views.py
+++ tree_menu/views.py
@@ -1,7 +1,25 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+
+from tree_menu.models import Page
 
 
 class Index(TemplateView):
     """Представление домашняя страница"""
 
     template_name = "tree_menu/index.html"
+
+
+class PageDetail(DetailView):
+    """Представление страниц для проверки """
+
+    model = Page
+    template_name = "tree_menu/page_2.html"
+    context_object_name = 'page'
+
+
+class ListDetail(TemplateView):
+    """Представление страниц для проверки """
+
+    model = Page
+    template_name = "tree_menu/page.html"
+    context_object_name = 'page'
